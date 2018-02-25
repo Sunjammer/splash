f54fb82e42e32f708a597289de2fd6e92c7a9f37
Andreas Kennedy <andreas@doomsday.no>
2018-02-25 20:37:11 +0100

    Initial import

---
+++ dist/Splash.js
@@ -0,0 +1,127 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const colors = require("colors/safe");
+const Commander = require("commander");
+const Configstore = require("configstore");
+const Confirm = require("prompt-confirm");
+const FS = require("fs-extra");
+const Path = require("path");
+const pkg = require('../package.json');
+// create a Configstore instance with an unique ID e.g. 
+// Package name and optionally some default values 
+const conf = new Configstore(pkg.name);
+function isDirEmpty(dir) {
+    var files = FS.readdirSync(dir);
+    return files.length == 0;
+}
+class Splash {
+    static spawn(source, target) {
+        if (!FS.pathExistsSync(source)) {
+            throw 'Template source directory does not exist';
+        }
+        if (typeof target === "string") {
+            target = process.cwd() + Path.sep + target;
+            console.log(colors.gray("Creating directory " + target));
+            FS.ensureDirSync(target);
+        }
+        else {
+            target = process.cwd();
+        }
+        const options = { overwrite: false };
+        let abort = false;
+        console.log(colors.gray("About to copy ") + colors.white(source) + " into " + colors.white(target));
+        if (!isDirEmpty(target)) {
+            console.log(colors.yellow("Target directory not empty!"));
+            new Confirm("Abort?").ask(answer => {
+                if (!answer) {
+                    new Confirm("Overwrite?").ask(answer => {
+                        if (answer) {
+                            console.log(colors.yellow('Overwriting duplicate files if any...'));
+                            options.overwrite = true;
+                        }
+                        else {
+                            console.log(colors.yellow('Keeping duplicate files if any...'));
+                        }
+                        FS.copySync(source, target, options);
+                        console.log(colors.green('Done'));
+                    });
+                }
+                else {
+                    console.log(colors.red('Aborted'));
+                }
+            });
+        }
+        else {
+            FS.copySync(source, target, options);
+            console.log(colors.green('Done'));
+        }
+    }
+    static run() {
+        Commander
+            .name("splash")
+            .version('0.0.1')
+            .usage('<template> [project]| map <name> | list')
+            .description('Splash or define a template')
+            .action((name, tgt) => {
+            if (conf.has('list')) {
+                let c = conf.get('list');
+                for (let k in c) {
+                    if (k == name) {
+                        Splash.spawn(c[k], tgt);
+                        return;
+                    }
+                }
+            }
+            console.error("No such template");
+        });
+        Commander.command('map <name>')
+            .description('Map cwd to template name')
+            .action(arg => {
+            let c = {};
+            if (conf.has('list'))
+                c = conf.get('list');
+            let target = process.cwd();
+            for (let k in c) {
+                if (c[k] == target) {
+                    delete (c[k]);
+                    console.log(colors.red("Removed duplicate (" + k + ")"));
+                }
+            }
+            c[arg] = target;
+            conf.set({ list: c });
+            console.log(colors.green("Mapped " + c[arg] + " to '" + arg + "'"));
+        });
+        Commander.command('unmap <name>')
+            .description('Forget a template')
+            .action(arg => {
+            let c = {};
+            if (conf.has('list'))
+                c = conf.get('list');
+            delete (c[arg]);
+            conf.set({ list: c });
+            console.log(colors.green("Unmapped '" + arg + "'"));
+        });
+        Commander.command('clear')
+            .description('Forget all templates')
+            .action(() => {
+            conf.clear();
+            console.log(colors.green("Done"));
+        });
+        Commander.command('list')
+            .description('List mapped templates')
+            .action(args => {
+            if (conf.has('list')) {
+                const l = conf.get('list');
+                console.log('Template mappings:');
+                for (let k in l) {
+                    console.log("\t" + colors.green(k) + " => " + colors.magenta(l[k]));
+                }
+            }
+        });
+        if (!process.argv.slice(2).length) {
+            Commander.outputHelp();
+        }
+        Commander.parse(process.argv);
+    }
+}
+exports.default = Splash;
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Splash_1 = require("./Splash");
+Splash_1.default.run();
